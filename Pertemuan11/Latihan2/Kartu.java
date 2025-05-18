02d23e2456dd8de7a50c62d0abc5c760afb42584
FaizKhairann <faizkhairan6@gmail.com>
2025-05-18 14:27:39 +0700

    P11

--- Pertemuan11/Latihan2/Kartu.java
+++ Pertemuan11/Latihan2/Kartu.java
@@ -1,7 +1,13 @@
 package Pertemuan11.Latihan2;
 
+// Interface Kartu, sebagai kontrak yang harus diimplementasikan oleh kelas yang menggunakan interface ini
 public interface Kartu {
+
+  // Method otentikasi: menerima input PIN dan mengembalikan nilai boolean
+  // true jika otentikasi berhasil, false jika gagal
   public boolean otentikasi(String pin);
 
+  // Method encode: menerima input PIN dan mengembalikan versi terenkripsi atau
+  // terformat dari PIN tersebut
   public String encode(String pin);
 }
